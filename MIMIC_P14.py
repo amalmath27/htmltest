600e7eb447aefee2e4a154b6cf1f5f3dc4e93cd1
Amal Mathew <amalmath27@gmail.com>
2021-06-30 09:26:31 +0530

    python compilation error fix

--- MIMIC_P14.py
+++ MIMIC_P14.py
@@ -198,56 +198,7 @@ def getStatus(logFile, txEnable):
         (scan_result[332] == '1') or(scan_result[333] == '1') or(scan_result[334] == '1') or(scan_result[335] == '1')):
 
         RLY4.on()   # R3 ON
-    """
-    if((scan_result[131] == '0')  and  (scan_result[130] == '0') and (scan_result[129] == '0') and (scan_result[128] == '0') and
-        (scan_result[127] == '0') and(scan_result[126] == '0') and(scan_result[125] == '0') and(scan_result[123] == '0') and
-        (scan_result[122] == '0') and (scan_result[121] == '0')and(scan_result[120] == '0') and(scan_result[119] == '0') and
-        (scan_result[118] == '0') and(scan_result[117] == '0') and(scan_result[116] == '0') and(scan_result[115] == '0') and
-        (scan_result[114] == '0') and(scan_result[147] == '0') and(scan_result[148] == '0') and(scan_result[149] == '0') and
-        (scan_result[150] == '0') and(scan_result[151] == '0') and(scan_result[152] == '0') and(scan_result[153] == '0') and
-        (scan_result[155] == '0') and(scan_result[156] == '0') and(scan_result[157] == '0') and(scan_result[158] == '0') and
-        (scan_result[159] == '0') and(scan_result[160] == '0') and(scan_result[161] == '0') and(scan_result[162] == '0') and
-        (scan_result[163] == '0') and(scan_result[164] == '0') and(scan_result[113] == '0') and(scan_result[112] == '0') and
-        (scan_result[195] == '0') and(scan_result[194] == '0') and(scan_result[193] == '0') and(scan_result[191] == '0') and
-        (scan_result[190] == '0') and(scan_result[189] == '0') and(scan_result[188] == '0') and(scan_result[187] == '0') and
-        (scan_result[186] == '0') and(scan_result[185] == '0') and(scan_result[184] == '0') and(scan_result[183] == '0') and
-        (scan_result[182] == '0') and(scan_result[165] == '0') and(scan_result[166] == '0') and(scan_result[167] == '0') and
-        (scan_result[196] == '0') and(scan_result[197] == '0') and(scan_result[199] == '0') and(scan_result[200] == '0') and
-        (scan_result[201] == '0') and(scan_result[202] == '0') and(scan_result[203] == '0') and(scan_result[204] == '0') and
-        (scan_result[205] == '0') and(scan_result[206] == '0') and(scan_result[207] == '0') and(scan_result[208] == '0') and
-        (scan_result[181] == '0') and(scan_result[180] == '0') and(scan_result[179] == '0') and(scan_result[178] == '0') and
-        (scan_result[177] == '0') and(scan_result[176] == '0') and(scan_result[173] == '0') and(scan_result[172] == '0') and
-        (scan_result[171] == '0') and(scan_result[170] == '0') and(scan_result[169] == '0') and(scan_result[168] == '0') and
-        (scan_result[251] == '0') and(scan_result[250] == '0') and(scan_result[249] == '0') and(scan_result[248] == '0') and
-        (scan_result[209] == '0') and(scan_result[210] == '0') and(scan_result[211] == '0') and(scan_result[212] == '0') and
-        (scan_result[213] == '0') and(scan_result[214] == '0') and(scan_result[217] == '0') and(scan_result[218] == '0') and
-        (scan_result[219] == '0') and(scan_result[220] == '0') and(scan_result[221] == '0') and(scan_result[222] == '0') and
-        (scan_result[223] == '0') and(scan_result[252] == '0') and(scan_result[253] == '0') and(scan_result[254] == '0') and
-        (scan_result[247] == '0') and(scan_result[246] == '0') and(scan_result[245] == '0') and(scan_result[244] == '0') and
-        (scan_result[243] == '0') and(scan_result[242] == '0') and(scan_result[241] == '0') and(scan_result[240] == '0') and
-        (scan_result[239] == '0') and(scan_result[238] == '0') and(scan_result[237] == '0') and(scan_result[236] == '0') and
-        (scan_result[235] == '0') and(scan_result[234] == '0') and(scan_result[233] == '0') and(scan_result[232] == '0') and
-        (scan_result[231] == '0') and(scan_result[255] == '0') and(scan_result[256] == '0') and(scan_result[257] == '0') and
-        (scan_result[258] == '0') and(scan_result[259] == '0') and(scan_result[260] == '0') and(scan_result[261] == '0') and
-        (scan_result[262] == '0') and(scan_result[263] == '0') and(scan_result[264] == '0') and(scan_result[265] == '0') and
-        (scan_result[266] == '0') and(scan_result[267] == '0') and(scan_result[268] == '0') and(scan_result[269] == '0') and
-        (scan_result[270] == '0') and(scan_result[271] == '0') and(scan_result[229] == '0') and(scan_result[228] == '0') and
-        (scan_result[227] == '0') and(scan_result[226] == '0') and(scan_result[225] == '0') and(scan_result[224] == '0') and
-        (scan_result[305] == '0') and(scan_result[304] == '0') and(scan_result[303] == '0') and(scan_result[302] == '0') and
-        (scan_result[301] == '0') and(scan_result[300] == '0') and(scan_result[299] == '0') and(scan_result[298] == '0') and
-        (scan_result[297] == '0') and(scan_result[296] == '0') and(scan_result[273] == '0') and(scan_result[274] == '0') and
-        (scan_result[275] == '0') and(scan_result[276] == '0') and(scan_result[277] == '0') and(scan_result[278] == '0') and
-        (scan_result[309] == '0') and(scan_result[310] == '0') and(scan_result[311] == '0') and(scan_result[312] == '0') and
-        (scan_result[313] == '0') and(scan_result[314] == '0') and(scan_result[315] == '0') and(scan_result[316] == '0') and
-        (scan_result[317] == '0') and(scan_result[318] == '0') and(scan_result[295] == '0') and(scan_result[294] == '0') and
-        (scan_result[293] == '0') and(scan_result[292] == '0') and(scan_result[291] == '0') and(scan_result[290] == '0') and
-        (scan_result[289] == '0') and(scan_result[288] == '0') and(scan_result[287] == '0') and(scan_result[286] == '0') and
-        (scan_result[285] == '0') and(scan_result[284] == '0') and(scan_result[283] == '0') and(scan_result[282] == '0') and
-        (scan_result[281] == '0') and(scan_result[280] == '0') and(scan_result[363] == '0') and(scan_result[319] == '0') and
-        (scan_result[320] == '0') and(scan_result[321] == '0') and(scan_result[322] == '0') and(scan_result[323] == '0') and
-        (scan_result[324] == '0') and(scan_result[325] == '0') and(scan_result[326] == '0') and(scan_result[327] == '0') and
-        (scan_result[328] == '0') and(scan_result[329] == '0') and(scan_result[330] == '0') and(scan_result[331] == '0') and
-        (scan_result[332] == '0') and(scan_result[333] == '0') and(scan_result[334] == '0') and(scan_result[335] == '0')): """
+    
     #if there is no fault 
     else: 
         RLY4.off()   #R3 OFF
